8a8a75e9932cabbf3890b3442a3681457c6f7606
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-17 23:04:21 -0500

    Added More Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setfeatPupId] = useState(null)
+
   console.log(puppies)
 
   return (
     <div>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={()=>{setfeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
+      <p>{featPupId}</p>
       </div>
   )
 }
