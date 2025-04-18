afd27e0fae1cb219cbc13facf3dc7d68a7e68b0d
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-18 00:06:08 -0500

    Added CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { puppyList } from "./data.js"
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
@@ -11,22 +12,35 @@ function App() {
 
   return (
     <div>
-      {
-        puppies.map((puppy) => {
-          return <p onClick={()=>{setfeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
-        })
-      }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => {setfeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
       {featPupId && (
         <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
+          <hr/>
+          <div className="featurePupDiv">
+            <h3>{featuredPup.name}</h3>
+            <ul className="puppyDataList">
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+              <li>Cute?: {featuredPup.isCute}</li>
+              <li>Tricks: 
+                <ul>{featuredPup.tricks.map((trick)=>{
+                  return (
+                    <li>{trick.title}</li>
+                  )
+              })}</ul>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
-      </div>
-  )
+    </div>
+  );
 }
 
 export default App
