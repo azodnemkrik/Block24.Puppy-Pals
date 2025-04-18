3eaba65184f44f0b081c3d6ddcbdf4e3905031df
Kirk Mendoza <kirkmendoza@gmail.com>
2025-04-17 23:18:53 -0500

    Rendered the Puppy Data

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { useState } from 'react'
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setfeatPupId] = useState(null)
-
   console.log(puppies)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log("featuredPup", featuredPup)
 
   return (
     <div>
@@ -14,7 +16,15 @@ function App() {
           return <p onClick={()=>{setfeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
-      <p>{featPupId}</p>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
   )
 }
